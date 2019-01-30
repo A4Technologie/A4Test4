21d97486265afd195e1e23a2c9e71e3ef085b09d
A4Technologie <techno@a4.fr>
2019-01-30 16:52:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,26 @@
 
 /**
+  * Enumeration of movements.
+  */
+enum RobotMoves
+{
+    //% block="Avancer"
+    Forward,
+    //% block="Reculer"
+    Backward,
+     //% block="Pivoter vers la droite"
+    RotateLeft,
+     //% block="Pivoter vers la gauche"
+    RotateRight,
+     //% block="Tourner à droite"
+    TurnRight,
+     //% block="Tourner à gauche"
+    TurnLeft
+ 
+}
+
+
+/**
  * Functions to operate Grove module.
  */
 //% weight=10 color=#00478F icon="\uf108" block="A4Test"
@@ -13,3 +34,19 @@ namespace A4Test1 {
         
     }
 }
+    
+    
+    /**
+ * Functions to operate Grove module.
+ */
+//% weight=10 color=#E9478F icon="\uf108" block="A4Test"
+namespace A4Test2 {
+    //% blockId="bitbot_motor_forward" block="$move à la vitesse $speed\\%"
+    //% speed.shadow="speedPicker"
+    //% weight=100
+     //% subcategory=Moteurs
+    export function drive(move:RobotMoves, speed: number) : void
+    {
+    }
+}
+
