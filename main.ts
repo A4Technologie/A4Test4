22e2d2a6430ec5405a47132b0a173f2842b5195f
A4Technologie <techno@a4.fr>
2019-01-30 16:30:13 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Functions to operate Grove module.
+ */
+//% weight=10 color=#000000 icon="\uf108" block="A4Test"
+namespace A4Test1 {
+    /**
+
+     */
+    //% blockId=grove_gesture_create_event block="Si tout va bien ca affiche"
+    export function onGesture( => void) {
+
+    }
+}
