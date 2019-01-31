5dbf44d0bd2c5fb5e037fbe0809a32ad406eddff
A4Technologie <techno@a4.fr>
2019-01-31 09:06:43 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,20 +20,6 @@ enum RobotMoves
 }
 
 
-/**
- * Functions to operate Grove motors.
- */
-//% weight=10 color=#00478F icon="\uf108" block="A4Test"
-namespace A4Test1 {
-    //% blockId="bitbot_motor_forward" block="Avancer à la vitesse speed %speed"
-    //% speed.shadow="speedPicker"
-    //% weight=100
-    //% subcategory=Motors
-    export function drive(speed: number): void
-    {
-        
-    }
-}
     
     
     /**
@@ -47,6 +33,30 @@ namespace A4Test2 {
      //% subcategory=Moteurs
     export function changementVitesse(move: RobotMoves, speed: number) : void
     {
+        let forward = (speed >= 0);
+        let absSpeed = Math.abs(speed);
+             if (speed > 100)
+            speed = 100;
+        else if (speed < -100)
+            speed = -100;
+         setPWM();
+       let speedPositive = speed*10*forward;
+       let speedNegative = speed*10*(!forward);
+     
+
+        if ((move == RobotMoves.Forward) || (move == RobotMoves.RotateLeft) || (move == RobotMoves.TurnLeft))
+            pins.analogWritePin(AnalogPin.P13, speedPositive);
+            pins.analogWritePin(AnalogPin.P14, speedNegative);  
+         if ((move == RobotMoves.Backward) || (move == RobotMoves.RotateRight))
+            pins.analogWritePin(AnalogPin.P13, speedNegative);
+            pins.analogWritePin(AnalogPin.P14, speedPositive);  
+
+         if ((move == RobotMoves.Forward) || (move == RobotMoves.RotateRight) || (move == RobotMoves.TurnRight))
+            pins.analogWritePin(AnalogPin.P15, speedPositive);
+            pins.analogWritePin(AnalogPin.P16, speedNegative);  
+         if ((move == RobotMoves.Backward) || (move == RobotMoves.RotateLeft))
+            pins.analogWritePin(AnalogPin.P15, speedNegative);
+            pins.analogWritePin(AnalogPin.P16, speedPositive);  
     }
 }
 
